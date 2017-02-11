0f062f9366ceb1bf295bf88c4e236cf06c52b123
Samuel Laycock <samuel.paul.laycock@gmail.com>
2017-02-11 10:25:25 +0000

    Update icons.

--- docs/dist/js/bootstrap.js
+++ docs/dist/js/bootstrap.js
@@ -1,6 +1,6 @@
 /*!
  * Bootstrap v3.3.7 (http://getbootstrap.com)
- * Copyright 2011-2016 Twitter, Inc.
+ * Copyright 2011-2017 Twitter, Inc.
  * Licensed under the MIT license
  */
 
@@ -921,7 +921,6 @@ if (typeof jQuery === 'undefined') {
  * Copyright 2011-2016 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
-
 
 +function ($) {
   'use strict';
